b2ae436f364cf3d2b292620ee22be2d07a006b30
Edgar Alexander Tellez Garcia <Edgar.Tellez@IQ-Online.com>
2023-08-09 15:13:58 -0500

    terminacion ejercicios

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*
+
 //1. Crea un programa que pregunte al usuario un número. 
 //Mostrar los números que son múltiplos de 5 desde 1 hasta el número introducido por el usuario.
 
@@ -43,7 +43,7 @@ while(x <= 50)
 }
 console.log("EJERCICIO 2: \n" + lista)
 
-*/
+
 //3. Crea un programa que solicite al usuario números, si lo que este introduce es un número guardarlo en un arreglo. 
 //Para terminar el capturar el usuario debe ingresar el número 0. Finalmente mostrar la lista de números capturados en pantalla o en la consola.
 
@@ -70,10 +70,40 @@ while(y <= cantidadDatos)
 // Mostramos el contenido del arreglo
 console.log ("Ejercicio 3: \n El arreglo es " + Arreglonumeros);
 
-//4. Crea un programa que solicite al usuario letras o palabras, si es así guardar el resultado. Para terminar de capturar el usuario no debe escribir valor alguno. Al terminar de capturar, mostrar en pantalla la concatenación de todas las palabras capturadas.
-
+//4. Crea un programa que solicite al usuario letras o palabras, si es así guardar el resultado. 
+//Para terminar de capturar el usuario no debe escribir valor alguno. 
+//Al terminar de capturar, mostrar en pantalla la concatenación de todas las palabras capturadas.
+let resultado = "";
+while(true)
+{
+    let dato = prompt("Ejercicio 4: Por favor ingresar letras o palabras:");
+    if(dato == "")
+    {
+        break;
+    }
+    else
+    {
+        resultado +=  dato + " ";
+    }    
+}
+console.log("Ejercicio 4: \n La concatenación de todas las palabras es: " + resultado);
 
 
 //5. Crea un programa que solicite al usuario un día de la semana (ej: lunes, jueves, domingo, etc). 
 //El programa mostrará un mensaje personalizado para cada día de la semana por medio de un alert. 
-//Y seguirá pidiendo al usuario introducir otro día. En caso de que el día introducido sea domingo mostrar al usuario el mensaje “Ve a descansar” y terminar la captura de información.
+//Y seguirá pidiendo al usuario introducir otro día. 
+//En caso de que el día introducido sea domingo mostrar al usuario el mensaje “Ve a descansar” y terminar la captura de información.
+let resultado1 = "";
+while(true)
+{
+    let dato1 = prompt("Ejercicio 5: Por favor ingresar un día de la semana (ej: lunes, jueves, domingo, etc):");
+    if(dato1 == "DOMINGO")
+    {
+        alert("DOMINGO: Ve a descansar");
+        break;
+    }
+    else
+    {
+        alert("INGRESO EL DIA: " + dato1);
+    }    
+}
